742697deb3854459dde922dfd3337dbcfbdff3ad
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-09-01 14:07:36 +0300

    ui-pilot > dev: control

---
+++ packages/ui-pilot/ui-pilot/control.py
@@ -0,0 +1,21 @@
+import pyautogui as pg
+import time
+
+
+
+def main():
+    print("pyautogui")
+    pg.moveTo(100, 70, duration = 1)
+    pg.mouseDown()
+    pg.click()
+    time.sleep(0.5)
+    pg.moveTo(150, 70, duration = 1)
+    pg.mouseUp()
+
+    # pg.keyDown('shift')
+    # pg.press('f')
+    # pg.keyUp('shift')
+
+
+if __name__ == '__main__':
+    main()
