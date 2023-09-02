75b9d24fe22372b814c6e0e7fd3e2b1b13cc7003
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-09-02 06:26:33 +0300

    ui-pilot > dev: control

--- packages/ui-pilot/ui-pilot/control.py
+++ packages/ui-pilot/ui-pilot/control.py
@@ -5,12 +5,14 @@ import time
 
 def main():
     print("pyautogui")
-    pg.moveTo(100, 70, duration = 1)
-    pg.mouseDown()
-    pg.click()
-    time.sleep(0.5)
-    pg.moveTo(150, 70, duration = 1)
-    pg.mouseUp()
+    # pg.scroll(-40)
+
+    # pg.moveTo(100, 70, duration = 1)
+    # pg.mouseDown()
+    # pg.click()
+    # time.sleep(0.5)
+    # pg.moveTo(150, 70, duration = 1)
+    # pg.mouseUp()
 
     # pg.keyDown('shift')
     # pg.press('f')
